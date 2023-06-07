9dc78ef44c0237d54e227eeca1661667b0e6562c
cristi <scarlat.cristian.gabriel@gmail.com>
2023-06-07 19:32:04 +0300

    addes style for Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
 const Header = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <div className="header__icon">
+        <img src="./src/assets/logo.svg" alt="logo" />
+      </div>
+
+      <div className="header__links">
+        <a href="#" className="header__links--link">
+          Chrome Extension
+        </a>
+        <a href="#" className="header__links--link">
+          Price Comparison
+        </a>
+        <a href="#" className="header__links--link">
+          Blog
+        </a>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- src/components/MainSectionForm.jsx
+++ src/components/MainSectionForm.jsx
@@ -3,26 +3,30 @@
 const MainSectionForm = () => {
   return (
     <form className="main-section__form">
-      <h1 className="main-section__form--headline headline">
-        REFERRAL FRIENDS AND GET REWARDS
-      </h1>
+      <div className="main-section__form--container">
+        <h1 className="main-section__form--headline headline">
+          REFERRAL FRIENDS AND GET REWARDS
+        </h1>
 
-      <p className="main-section__form--text">
-        Refer your friends to us and earn hotel booking vouchers. We'll give you
-        1 coin or each friend that installs our extension. Minimum cash-out at
-        20 coins.
-      </p>
+        <p className="main-section__form--text">
+          Refer your friends to us and earn hotel booking vouchers. We'll give
+          you 1 coin or each friend that installs our extension. Minimum
+          cash-out at 20 coins.
+        </p>
 
-      <div className="main-section__form--input-btn">
-        <input
-          type="text"
-          placeholder="Enter your email address"
-          className="main-section__form--input"
-        />
-        <button className="main-section__form--btn">Get Referral Link</button>
+        <div className="main-section__form--input-btn">
+          <input
+            type="text"
+            placeholder="Enter your email address"
+            className="main-section__form--input"
+          />
+          <button className="main-section__form--btn">Get Referral Link</button>
+        </div>
+
+        <p className="main-section__form--limits">
+          Limits on max rewards apply.
+        </p>
       </div>
-
-      <p className="main-section__form--limits">Limits on max rewards apply</p>
     </form>
   );
 };
